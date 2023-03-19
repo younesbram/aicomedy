ff85751b42ac073a312b8ef11a38d2980844afc7
Younes Brahimi <43382860+younesbram@users.noreply.github.com>
2023-03-19 17:12:34 -0400

    new emoji

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 st.set_page_config(
     page_title="AI Comedy",
-    page_icon="🎭",
+    page_icon="😂",
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
